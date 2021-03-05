6789c07ad92ed4e68b1d7567b0ecf785235fefbd
filipetrinidad <fipe5766@gmail.com>
2021-03-05 19:02:25 -0300

    stop words

---
+++ emocoes.py
@@ -0,0 +1,32 @@
+import nltk
+
+base = [('Eu estou muito feliz', 'alegria'),
+        ('Nós estamos alegres', 'alegria'),
+        ('Me sinto amavel', 'alegria'),
+        ('Me considero afortunado', 'alegria'),
+        ('Sou prospero', 'alegria'),
+        ('Me diverti muito', 'alegria'),
+        ('Estou alegre por voce', 'alegria'),
+        ('Acordei muito bem', 'alegria'),
+        ('ele está atimo', 'alegria'),
+        ('Hoje é um otimo dia para ser feliz', 'alegria'),
+        ('Estou apavorado', 'medo'),
+        ('Sinto medo disso', 'medo'),
+        ('Isso me causa aflição', 'medo'),
+        ('Me assustei', 'medo'),
+        ('Que aflicao', 'medo'),
+        ('Cuidado com os males', 'medo'),
+        ('Estou tremendo de tanto medo', 'medo'),
+        ('Você está assustado', 'medo'),
+        ('que pavor', 'medo'),
+        ('voce e um covarde', 'medo')]
+
+stopwordsnltk = nltk.corpus.stopwords.words('portuguese')
+
+def removestopwords(text):
+        frases = []
+        for(palavras, emocoes) in base:
+                semstopwords = [p for p in palavras.split() if p not in stopwordsnltk]
+                frases.append((semstopwords, emocoes))
+                return frases
+print(removestopwords(base))
